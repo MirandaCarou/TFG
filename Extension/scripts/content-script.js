ed13331387cdbea328c75b9bbeb3da1e95e4ace9
MirandaCarou <miranda.carou22boiro@gmail.com>
2022-09-17 20:57:57 +0200

    Sale un cuadradito rojo y amazon xd

--- Extension/scripts/content-script.js
+++ Extension/scripts/content-script.js
@@ -1,1 +1,20 @@
-window.open('popup.htm',"ventana-poup",'width=400,height=250');
+//window.open('popup.htm',"ventana-poup",'width=400,height=250');
+
+function abrirpoup(){
+    window.open('https://www.amazon.es/',"ventana-poup",'width=400,height=250');
+    //document.getElementById('fusion-app').style.display = "none"
+}
+abrirpoup();
+
+const popup = document.createElement("div");
+popup.style.backgroundColor = "red";
+popup.id = "popup";
+popup.style.position = "absolute";
+popup.style.height = "500px";
+popup.style.width = "500px";
+popup.textContent = "Esto es un div insertado con JS.";
+
+const app = document.querySelector("#fusion-app"); // <div id="app">App</div>
+
+app.insertAdjacentElement("beforebegin", popup);
+//document.getElementsByClassName('ctn_head').inner;
