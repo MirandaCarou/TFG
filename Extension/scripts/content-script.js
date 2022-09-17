34518751ddd055205090377c96366c74e2e81f67
Miranda <71145273+MirandaCarou@users.noreply.github.com>
2022-09-17 18:12:18 +0200

    Add files via upload

---
+++ Extension/scripts/content-script.js
@@ -0,0 +1,2 @@
+//document.getElementsByTagName("body")[0].style.backgroundImage = "url('/icons/semillas-de-marihuana.png')"; 
+document.getElementsByTagName("body")[0].style.backgroundColor = "pink"; 
