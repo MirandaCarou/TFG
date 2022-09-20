e7d9a4c1fc8bc01ba20f813ff89abff0a0395b73
MirandaCarou <miranda.carou22boiro@gmail.com>
2022-09-20 16:00:03 +0200

    Acepta cookies elPais, da un error, fallos

--- Extension/scripts/content-script.js
+++ Extension/scripts/content-script.js
@@ -1,27 +1,35 @@
 //window.open('popup.htm',"ventana-poup",'width=400,height=250');
-
+/* 
 function abrirpoup(){
     window.open('https://www.amazon.es/',"ventana-poup",'width=400,height=250');
     //document.getElementById('fusion-app').style.display = "none"
 }
 abrirpoup();
+*/
 
-const popup = document.createElement("div");
-popup.style.backgroundColor = "blue"
-popup.id = "popup";
-popup.style.marginLeft = "35%";
-popup.style.backgroundImage = "url('imagen.png')";
-popup.style.zIndex = "10000";
-popup.style.position = "absolute";
-popup.style.height = "500px";
-popup.style.width = "500px";
-popup.textContent = "Esto es un div insertado con JS.";
-
-const app = document.querySelector("#fusion-app");
-app.insertAdjacentElement("beforebegin", popup);
-
+/*
 //const infocookie = document.querySelector("#didomi-host");
  var a = document.getElementById("didomi-host");
  a.style.display = "none";
+*/
+function mostrarPopup() {
+    const popup = document.createElement("div");
+    popup.style.backgroundColor = "red"
+    popup.id = "popup";
+    popup.style.marginLeft = "35%";
+    popup.style.backgroundImage = "url('imagen.png')";
+    popup.style.zIndex = "10000";
+    popup.style.position = "absolute";
+    popup.style.height = "250px";
+    popup.style.width = "250px";
+    popup.textContent = "Enorabuena se han aceptado las cookies";
+    const app = document.querySelector("#fusion-app");
+    app.insertAdjacentElement("beforebegin", popup);
+}
 
-
+function aceptarCookies(){
+    var a = document.getElementById("didomi-notice-agree-button");
+    a.click();
+    mostrarPopup();
+}
+aceptarCookies();
